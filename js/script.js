7036aa562ee99b699ba8b93f292cbbf7b5477229
natacha-felisberto <natacha_felisberto@yahoo.com.br>
2026-08-24 22:33:39 -0300

    adicionando o js do botão voltar das trilhas

--- js/script.js
+++ js/script.js
@@ -1,28 +1,40 @@
+const botaoAnterior = document.getElementById("trilha-anterior");
 const botaoProximo = document.getElementById("trilha-proxima");
 
 const trilhas = document.querySelectorAll(".card-trilha");
-
 const trilhasGrid = document.querySelector(".trilhas-grid");
 
 let trilhaAtual = 0;
 
 const limite = trilhas.length - 3;
 
-console.log(trilhas);
-console.log(trilhasGrid);
 
+// BOTÃO PRÓXIMO
 
 botaoProximo.addEventListener("click", function() {
 
-     if (trilhaAtual < limite) {
+    if (trilhaAtual < limite) {
 
-    console.log("Clicou na seta!");
+        trilhaAtual++;
 
-    trilhaAtual++;
+        trilhasGrid.style.transform =
+            `translateX(-${trilhaAtual * 33.33}%)`;
 
-    trilhasGrid.style.transform =
-        `translateX(-${trilhaAtual * 33.33}%)`;
+    }
 
+});
+
+
+// BOTÃO ANTERIOR
+
+botaoAnterior.addEventListener("click", function() {
+
+    if (trilhaAtual > 0) {
+
+        trilhaAtual--;
+
+        trilhasGrid.style.transform =
+            `translateX(-${trilhaAtual * 33.33}%)`;
 
     }
 
